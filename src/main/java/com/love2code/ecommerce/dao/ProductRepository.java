c291e9d5037297a6da0e6dabf2c4b12b51d20d3f
alexmwaura <alexmwaura43@gmail.com>
2022-02-23 18:21:45 +0300

    jpa entities

---
+++ src/main/java/com/love2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.love2code.ecommerce.dao;
+
+import com.love2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
